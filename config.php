d5f2b27c8699abee37f82466685be4648b36dd05
dhanurjayboitai <124765521+dhanurjayboitai@users.noreply.github.com>
2024-07-29 09:56:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_DBNAME','db_crud');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DBNAME);
 if($con->mysqli_connect_error())
 {
-    echo "Faild connection to Mysqli:" .mysqli_connect_error();
+    echo "Faild connection to Mysqli:" . $con->mysqli_connect_error();
 }
 else{
     echo "connection success";
